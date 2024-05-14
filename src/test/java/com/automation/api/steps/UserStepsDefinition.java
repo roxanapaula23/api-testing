aa4ce76d51d5f5adda33090110a8cb18abee7403
roxanapaula23 <roxanasfabu@gmail.com>
2024-05-14 20:38:36 +0300

    Get details by username feature
    - Added new steps in User Steps Definition

--- src/test/java/com/automation/api/steps/UserStepsDefinition.java
+++ src/test/java/com/automation/api/steps/UserStepsDefinition.java
@@ -75,7 +75,8 @@ public class UserStepsDefinition {
     @When("I send a POST request with a list of users")
     public void iSendAPOSTRequestWithAListOfUsers() throws IOException {
         String json = new String(Files.readAllBytes(Paths.get("src/test/resources/data/users.json")));
-        List<UserModel> users = objectMapper.readValue(json, new TypeReference<>() {});
+        List<UserModel> users = objectMapper.readValue(json, new TypeReference<>() {
+        });
         Response response = HttpManager.post("/v2/user/createWithArray", users);
         HttpManager.setResponse(response);
     }
@@ -83,7 +84,8 @@ public class UserStepsDefinition {
     @When("I send a GET request to login")
     public void iSendAGETRequestToLogin() throws IOException {
         String json = new String(Files.readAllBytes(Paths.get("src/test/resources/data/users.json")));
-        List<UserModel> users = objectMapper.readValue(json, new TypeReference<>() {});
+        List<UserModel> users = objectMapper.readValue(json, new TypeReference<>() {
+        });
         String username = users.get(0).getUsername();
         String password = users.get(0).getPassword();
 
@@ -123,5 +125,73 @@ public class UserStepsDefinition {
 
         Response response = request.put("/v2/user/" + userToUpdate.getUsername());
         HttpManager.setResponse(response);
+    }
+
+    @Given("The system is operational")
+    public void theSystemIsOperational() {
+        Response response = RestAssured.get(baseURI);
+        Assert.assertEquals("Status code might be 503- Service Unavailable", 200, response.getStatusCode());
+    }
+
+    @Given("A list of users is registered in the system")
+    public void aListOfUsersIsRegisteredInTheSystem() throws IOException {
+        String json = new String(Files.readAllBytes(Paths.get("src/test/resources/data/users.json")));
+        List<UserModel> users = objectMapper.readValue(json, new TypeReference<>() {
+        });
+        Response response = HttpManager.post("/v2/user/createWithArray", users);
+        HttpManager.setResponse(response);
+    }
+
+    @When("I request information for a specific username")
+    public void iRequestInformationForASpecificUsername() throws IOException {
+        String json = new String(Files.readAllBytes(Paths.get("src/test/resources/data/users.json")));
+        List<UserModel> users = objectMapper.readValue(json, new TypeReference<>() {
+        });
+        String username = users.get(1).getUsername();
+        Response response = HttpManager.get("/v2/user/" + username);
+        HttpManager.setResponse(response);
+    }
+
+    @Then("The system responds with a {string} status code message")
+    public void theSystemRespondsWithAStatusCode(String statusMessage) {
+        int statusCode = 0;
+        switch (statusMessage) {
+            case "OK":
+                statusCode = 200;
+                break;
+            case "Not Found":
+                statusCode = 404;
+                break;
+        }
+        Response response = HttpManager.getResponse();
+        Assert.assertEquals("Expected status code is " + statusCode, statusCode, response.getStatusCode());
+    }
+
+    @And("The information matches the user details")
+    public void theInformationMatchesTheUserDetails() {
+        Response response = HttpManager.getResponse();
+        UserModel responseUser = response.getBody().as(UserModel.class);
+
+        Assert.assertEquals("Expected id to be 2", 2, responseUser.getId());
+        Assert.assertEquals("Expected username to be paulteru", "paulteru", responseUser.getUsername());
+        Assert.assertEquals("Expected firstName to be Paul", "Paul", responseUser.getFirstName());
+        Assert.assertEquals("Expected lastName to be Teru", "Teru", responseUser.getLastName());
+        Assert.assertEquals("Expected email to be paulteru@yopmail.com", "paulteru@yopmail.com", responseUser.getEmail());
+        Assert.assertEquals("Expected password to be P@ss123242!", "P@ss123242!", responseUser.getPassword());
+        Assert.assertEquals("Expected phone to be 0740122421", "0740122421", responseUser.getPhone());
+        Assert.assertEquals("Expected userStatus to be 2", 2, responseUser.getUserStatus());
+    }
+
+    @When("I attempt to retrieve an username that does not exist")
+    public void iAttemptToRetrieveAnUsernameThatDoesNotExist() {
+        String username = "nonExistentUsername";
+        Response response = HttpManager.get("/v2/user/" + username);
+        HttpManager.setResponse(response);
+    }
+
+    @And("The response message is {string}")
+    public void theResponseMessageIs(String message) {
+        Response response = HttpManager.getResponse();
+        Assert.assertTrue(response.getBody().asString().contains(message));
     }
 }
